31990cf9997dd7d67283a92deb47070ffc08480e
Suraiya-Hasan <suraiyahasan@iut-dhaka.edu>
2023-08-22 18:00:23 +0600

    fetching data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { WeatherService } from './services/weather.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'weatherApp';
+export class AppComponent implements OnInit, OnDestroy {
+  weatherSub!: Subscription;
+
+  constructor(private weatherService: WeatherService) { }
+  ngOnInit(): void {
+    this.weatherSub = this.weatherService.getWeatherData('Dhaka')
+      .subscribe({
+        next: (res) => {
+          console.log(res);
+        }
+      })
+  }
+  ngOnDestroy(): void {
+    this.weatherSub.unsubscribe();
+  }
 }
--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,1 +1,7 @@
-export const environment = {};
+export const environment = {
+    production: false,
+    weatherApiBaseURL: 'https://api.openweathermap.org/data/2.5/weather',
+    APIKey: 'd816067db38d127953d9677e2d01e036',
+};
+
+
